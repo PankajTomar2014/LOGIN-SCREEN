d99161f64454f098605253b34c03fe322af8cd70
Pankaj Tomar <59471669+PankajTomar2014@users.noreply.github.com>
2021-04-09 07:54:46 +0530

    Delete App.js

--- App.js
+++
@@ -1,9 +0,0 @@
-import React, {useEffect, Component} from 'react';
-import {LogBox} from 'react-native';
-import Navigator from './src/Navigation/Navigaton';
-LogBox.ignoreAllLogs();
-
-const App = () => {
-  return <Navigator />;
-};
-export default App;
